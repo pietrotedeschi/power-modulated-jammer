97687e97b85dd4db9eda803fa4af3c822673ae0e
Pietro Tedeschi <pietrotedeschi.me@gmail.com>
2021-03-21 07:23:05 +0100

    Add files via upload

---
+++ pmj.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Power-modulated Jammer
+# Author: Pietro Tedeschi
+# Description: Power-Modulated Jammer
+# GNU Radio version: v3.8.2.0-73-g4a84443c
+
+from gnuradio import analog
+from gnuradio import gr
+from gnuradio.filter import firdes
+import sys
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+import osmosdr
+import time
+
+import random
+
+
+class pmj(gr.top_block):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Power-modulated Jammer")
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 2e6
+        self.f0 = f0 = 2437e6
+        self.bw = bw = 5e6
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.osmosdr_sink_0 = osmosdr.sink(
+            args="numchan=" + str(1) + " " + "hackrf=0"
+        )
+        self.osmosdr_sink_0.set_time_unknown_pps(osmosdr.time_spec_t())
+        self.osmosdr_sink_0.set_sample_rate(samp_rate)
+        self.osmosdr_sink_0.set_center_freq(f0, 0)
+        self.osmosdr_sink_0.set_freq_corr(0, 0)
+        self.osmosdr_sink_0.set_gain(14, 0)
+        self.osmosdr_sink_0.set_if_gain(20, 0)
+        self.osmosdr_sink_0.set_bb_gain(20, 0)
+        self.osmosdr_sink_0.set_antenna('', 0)
+        self.osmosdr_sink_0.set_bandwidth(bw, 0)
+        self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, 1, 0)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_noise_source_x_0, 0), (self.osmosdr_sink_0, 0))
+
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.osmosdr_sink_0.set_sample_rate(self.samp_rate)
+
+    def get_f0(self):
+        return self.f0
+
+    def set_f0(self, f0):
+        self.f0 = f0
+        self.osmosdr_sink_0.set_center_freq(self.f0, 0)
+
+    def get_bw(self):
+        return self.bw
+
+    def set_bw(self, bw):
+        self.bw = bw
+        self.osmosdr_sink_0.set_bandwidth(self.bw, 0)
+
+    def pulse(self):
+        print('Press CTRL+C to stop the power-modulated jammer.')
+        while True:
+            time.sleep(1)
+            self.osmosdr_sink_0.set_if_gain(random.randint(0,40), 0)
+
+
+
+def main(top_block_cls=pmj, options=None):
+    tb = top_block_cls()
+    random.seed(0)
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    tb.start()
+
+    tb.pulse()
+    try:
+        input('Press Enter to quit: ')
+    except EOFError:
+        pass
+    tb.stop()
+    tb.wait()
+
+
+if __name__ == '__main__':
+    main()
